c246ac2a5dc14e0990bb883a39ad771eaa3ce5df
Nomris <54217772+Nomris@users.noreply.github.com>
2024-01-09 22:19:53 +0100

    Added Methods to Wrap Queries with a returning IDataReader

--- Org.Websn.Extensions.Database/IDbConnectionExtensions.cs
+++ Org.Websn.Extensions.Database/IDbConnectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Linq;
 
 
 namespace Org.Websn.Extensions
@@ -22,9 +23,8 @@ namespace Org.Websn.Extensions
         /// <summary>
         /// Executes the provided <paramref name="commandOperator"/>.
         /// </summary>
-        /// <param name="connection">The connection on which the <paramref name="commandOperator"/> should be executed</param>
         /// <param name="commandOperator">The operation that is to be made on the <paramref name="connection"/></param>
-        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. (Set false if the <paramref name="connection"/> is already open)</param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
         public static void WrapCommand(this IDbConnection connection, Action<IDbCommand> commandOperator, bool? openConnection = null)
         {
             if (connection == null) throw new ArgumentNullException(nameof(connection));
@@ -57,16 +57,15 @@ namespace Org.Websn.Extensions
         /// <summary>
         /// Executes the provided <paramref name="commandOperator"/>. Returns <see langword="null"/> on success. And an <see cref="Exception"/> if any occurred
         /// </summary>
-        /// <param name="connection">The connection on which the <paramref name="commandOperator"/> should be executed</param>
         /// <param name="commandOperator">The operation that is to be made on the <paramref name="connection"/></param>
-        /// <param name="openConnetcion">Wether the <paramref name="connection"/> should be opened. (Set false if the <paramref name="connection"/> is already open)</param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
         /// <param name="exc">The <see cref="Exception"/> if any was thrown, else it will be <see langword="null"/></param>
         /// <returns>Returns <see langword="true"/> if the command completed successfully, <see langword="false"/> otherwise</returns>
-        public static bool TryWrapCommand(this IDbConnection connection, Action<IDbCommand> commandOperator, out Exception exc, bool? openConnetcion = null)
+        public static bool TryWrapCommand(this IDbConnection connection, Action<IDbCommand> commandOperator, out Exception exc, bool? openConnection = null)
         {
             try
             {
-                WrapCommand(connection, commandOperator, openConnetcion);
+                WrapCommand(connection, commandOperator, openConnection);
 
                 exc = null;
                 return true;
@@ -81,9 +80,8 @@ namespace Org.Websn.Extensions
         /// <summary>
         /// Executes the provided <paramref name="commandOperator"/>.
         /// </summary>
-        /// <param name="connection">The connection on which the <paramref name="commandOperator"/> should be executed</param>
         /// <param name="commandOperator">The operation that is to be made on the <paramref name="connection"/></param>
-        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. (Set false if the <paramref name="connection"/> is already open)</param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
         public static TResult WrapCommand<TResult>(this IDbConnection connection, Func<IDbCommand, TResult> commandOperator, bool? openConnection = null)
         {
             if (connection == null) throw new ArgumentNullException(nameof(connection));
@@ -118,18 +116,17 @@ namespace Org.Websn.Extensions
         /// <summary>
         /// Executes the provided <paramref name="commandOperator"/>. Returns <see langword="null"/> on success. And an <see cref="Exception"/> if any occurred
         /// </summary>
-        /// <param name="connection">The connection on which the <paramref name="commandOperator"/> should be executed</param>
         /// <param name="commandOperator">The operation that is to be made on the <paramref name="connection"/></param>
-        /// <param name="openConnetcion">Wether the <paramref name="connection"/> should be opened. (Set false if the <paramref name="connection"/> is already open)</param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
         /// <param name="exc">The <see cref="Exception"/> if any was thrown, else it will be <see langword="null"/></param>
         /// <param name="result">The <typeparamref name="TResult"/></param>
         /// <param name="default">The default value that should be returend if the operation faild</param>
         /// <returns>Returns <see langword="true"/> if the command completed successfully, <see langword="false"/> otherwise</returns>
-        public static bool TryWrapCommand<TResult>(this IDbConnection connection, Func<IDbCommand, TResult> commandOperator, out TResult result, TResult @default, out Exception exc, bool? openConnetcion = null)
+        public static bool TryWrapCommand<TResult>(this IDbConnection connection, Func<IDbCommand, TResult> commandOperator, out TResult result, TResult @default, out Exception exc, bool? openConnection = null)
         {
             try
             {
-                result = WrapCommand(connection, commandOperator, openConnetcion);
+                result = WrapCommand(connection, commandOperator, openConnection);
 
                 exc = null;
                 return true;
@@ -138,6 +135,81 @@ namespace Org.Websn.Extensions
             {
                 exc = _exc;
                 result = @default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Executes the provided <paramref name="commandText"/> and returns a <see cref="IDataReader"/> as a result of the query
+        /// </summary>
+        /// <param name="commandText">The Command that is to be executed, use <see cref="string.Format"/> style inserstion of <paramref name="args"/></param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
+        /// <param name="args">The Arguments</param>
+        /// <returns>The <see cref="IDataReader"/> that represents the <paramref name="commandText"/></returns>
+        public static IDataReader WrapCommandAsReader(IDbConnection connection, string commandText, bool? openConnection, params object[] args)
+        {
+            if (connection == null) throw new ArgumentNullException(nameof(connection));
+            if (!openConnection.HasValue)
+                openConnection = !IsConnectionOpen(connection);
+
+            if (openConnection.Value && connection.IsConnectionOpen())
+                throw new InvalidOperationException("Cannot open connection that is already open");
+
+            try
+            {
+                if (openConnection.Value) connection.Open();
+
+                IDbCommand command = connection.CreateCommand();
+                int i = 0;
+                command.CommandText = string.Format(commandText, args.Select(arg => $"@_{i++}"));
+                i = -1;
+                while (++i < args.Length)
+                    command.AddGenericParameter($"@_{i}", args[i]);
+
+                return new DataReaderProxy(command.ExecuteReader(), () =>
+                {
+                    command.Dispose();
+                    if (openConnection.Value)
+                        try
+                        {
+                            connection.Close();
+                        }
+                        catch (Exception exc)
+                        {
+                            Console.WriteLine("Faild to close the connetcion");
+                            Console.Error.WriteLine(exc);
+                        }
+                });
+
+            }
+            finally
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// Executes the provided <paramref name="commandText"/> and returns a <see cref="IDataReader"/> as a result of the query. And an <see cref="Exception"/> if any occurred
+        /// </summary>
+        /// <param name="commandText">The Command that is to be executed, use <see cref="string.Format"/> style inserstion of <paramref name="args"/></param>
+        /// <param name="openConnection">Wether the <paramref name="connection"/> should be opened. Set <see langword="false"/> if the <paramref name="connection"/> is already open. Set <see langword="null"/> to auto determin</param>
+        /// <param name="args">The Arguments</param>
+        /// <param name="result">The <see cref="IDataReader"/> that represents the <paramref name="commandText"/></param>
+        /// <param name="exc">The <see cref="Exception"/> if any was thrown, else it will be <see langword="null"/></param>
+        /// <returns>Returns <see langword="true"/> if the command completed successfully, <see langword="false"/> otherwise</returns>
+        public static bool TryWrapCommandAsReader(IDbConnection connection, string commandText, out IDataReader result, out Exception exc, bool? openConnection, params object[] args)
+        {
+            try
+            {
+                result = WrapCommandAsReader(connection, commandText, openConnection, args);
+
+                exc = null;
+                return true;
+            }
+            catch (Exception _exc)
+            {
+                exc = _exc;
+                result = null;
                 return false;
             }
         }
