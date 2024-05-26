c08d0a7de19cadaf372fdb461eeb002f7b4c0948
Nomris <54217772+Nomris@users.noreply.github.com>
2024-05-26 15:23:11 +0200

    Added Enumreable version of the ReadAll and TryReadAll functions

--- Org.Websn.Extensions.Database/IDataReaderExtensions.cs
+++ Org.Websn.Extensions.Database/IDataReaderExtensions.cs
@@ -1,29 +1,13 @@
 ﻿using System;
 using System.Data;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Org.Websn.Extensions
 {
     public static class IDataReaderExtensions
     {
-        /// <summary>
-        /// Reads all records of the <see cref="IDataReader"/> and returns a <typeparamref name="T"/> for each entry
-        /// </summary>
-        /// <param name="readFunction">The function that will be used to process all records</param>
-        /// <param name="closeReader">Whether the <see cref="IDataReader"/> should be closed once all records where read</param>
-        public static T[] ReadAll<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, bool closeReader = true)
-        {
-            List<T> values = new List<T>();
-            while (reader.Read())
-                values.Add(readFunction(reader));
-
-            if (closeReader)
-            {
-                reader.Close();
-                reader.Dispose();
-            }
-            return values.ToArray();
-        }
+        #region No Return Values
 
         /// <summary>
         /// Reads all records of the <see cref="IDataReader"/>
@@ -41,6 +25,7 @@ namespace Org.Websn.Extensions
             }
         }
 
+
         /// <summary>
         /// Reads all records of the <see cref="IDataReader"/>
         /// </summary>
@@ -51,7 +36,7 @@ namespace Org.Websn.Extensions
         {
             try
             {
-                ReadAll(reader, readFunction, false);
+                ReadAll(reader, readFunction, closeReader);
 
                 exc = null;
                 return true;
@@ -60,6 +45,52 @@ namespace Org.Websn.Extensions
             {
                 exc = e;
                 return false;
+            }
+        }
+
+        #endregion
+
+        #region Static Buffer
+
+        /// <summary>
+        /// Reads all records of the <see cref="IDataReader"/> and returns a <typeparamref name="T"/> for each entry
+        /// </summary>
+        /// <param name="readFunction">The function that will be used to process all records</param>
+        /// <param name="closeReader">Whether the <see cref="IDataReader"/> should be closed once all records where read</param>
+        public static T[] ReadAll<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, bool closeReader = true)
+        {
+            return ReadAllEnumerable<T>(reader, readFunction, closeReader).ToArray();
+        }
+
+        /// <summary>
+        /// Reads all records of the <see cref="IDataReader"/> and returns a <typeparamref name="T"/> for each entry in <paramref name="result"/>
+        /// </summary>
+        /// <param name="readFunction">The function that will be used to process all records</param>
+        /// <param name="closeReader">Whether the <see cref="IDataReader"/> should be closed once all records where read</param>
+        /// <param name="exc">The <see cref="Exception"/> that was thrown druing execution, <see langword="null"/> if no <see cref="Exception"/> was thrown</param>
+        public static bool TryReadAll<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, out Exception exc, out T[] result, bool closeReader = true)
+        {
+            bool success = TryReadAllEnumerable<T>(reader, readFunction, out exc, out IEnumerable<T> internalResult, closeReader = true);
+            result = internalResult.ToArray();
+            return success;
+        }
+
+        #endregion
+
+        #region Enumrable
+
+
+        /// <summary>
+        /// Reads all records of the <see cref="IDataReader"/> and returns a <typeparamref name="T"/> for each entry
+        /// </summary>
+        /// <param name="readFunction">The function that will be used to process all records</param>
+        /// <param name="closeReader">Whether the <see cref="IDataReader"/> should be closed once all records where read</param>
+        public static IEnumerable<T> ReadAllEnumerable<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, bool closeReader = true)
+        {
+            try
+            {
+                while (reader.Read())
+                    yield return readFunction(reader);
             }
             finally
             {
@@ -77,11 +108,11 @@ namespace Org.Websn.Extensions
         /// <param name="readFunction">The function that will be used to process all records</param>
         /// <param name="closeReader">Whether the <see cref="IDataReader"/> should be closed once all records where read</param>
         /// <param name="exc">The <see cref="Exception"/> that was thrown druing execution, <see langword="null"/> if no <see cref="Exception"/> was thrown</param>
-        public static bool TryReadAll<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, out Exception exc, out T[] result, bool closeReader = true)
+        public static bool TryReadAllEnumerable<T>(this IDataReader reader, Func<IDataRecord, T> readFunction, out Exception exc, out IEnumerable<T> result, bool closeReader = true)
         {
             try
             {
-                result = ReadAll(reader, readFunction, false);
+                result = ReadAll(reader, readFunction, closeReader);
                 exc = null;
                 return true;
             }
@@ -91,14 +122,8 @@ namespace Org.Websn.Extensions
                 exc = e;
                 return false;
             }
-            finally
-            {
-                if (closeReader)
-                {
-                    reader.Close();
-                    reader.Dispose();
-                }
-            }
         }
+
+        #endregion
     }
 }
